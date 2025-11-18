bb6bf3367d7d2faa96e65d9855e91c06e1f57e9d
[이모션]강혜린 <hkang0420@gmail.com>
2025-11-18 10:55:23 +0900

    🎨 application-section-구조

--- components/main/Application.jsx
+++ components/main/Application.jsx
@@ -1,17 +1,53 @@
 "use client";
 import { useContext } from "react";
 import { LangContext } from "@/context/LangContext";
+import HomeAppliance from "@/public/images/content/application-HomeAppliance.png";
+import MicroLED from "@/public/images/content/application-MicroDisplay.png";
+import SensorData from "@/public/images/content/application-SensorData.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const Application = () => {
-  const { dict } = useContext(LangContext);
+  const { dict, lang } = useContext(LangContext);
+  const menuItems = [
+    {
+      title: "Home Appliance & Industrial",
+      description: dict?.main?.applications?.homeAppliance?.text,
+      link: `${lang}/application/appliance`,
+      img: HomeAppliance,
+    },
+    {
+      title: "MicroLED Display",
+      description: dict?.main?.applications?.microDisplay?.text,
+      link: `${lang}/application/display`,
+      img: MicroLED,
+    },
+    {
+      title: "Sensor & Data Comm.",
+      description: dict?.main?.applications?.sensorData?.text,
+      link: `${lang}/application/sensor`,
+      img: SensorData,
+    },
+  ];
 
   if (!dict?.main) return <p>Loading...</p>;
 
   return (
-    <section className="main-section">
+    <section className="main-section application">
       <div className="title-area">
         <h2 className="title">Applications</h2>
-        <p>{dict.main.applications.desc}</p>
+        <p className="desc">{dict.main.applications.desc}</p>
+      </div>
+
+      <div className="itme-list">
+        {menuItems.map((item, i) => {
+          return (
+            <Link href={item.link} key={i}>
+              {item.title} / {item.description}
+              <Image src={item.img} alt={item.title} />
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
