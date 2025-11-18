7c3345c2c44243694b3d2474b30e05ec505b15d5
[이모션]강혜린 <hkang0420@gmail.com>
2025-11-18 18:40:19 +0900

    css 수정중

--- components/main/AboutUs.jsx
+++ components/main/AboutUs.jsx
@@ -1,19 +1,41 @@
 "use client";
 import Counter from "@/components/AnimateText/Counter";
 import { LangContext } from "@/context/LangContext";
+import Link from "next/link";
 import { useContext } from "react";
 
 const AboutUs = () => {
-  const { dict } = useContext(LangContext);
+  const { dict, lang } = useContext(LangContext);
   return (
-    <section className="aboutus">
-      <p>{dict?.main?.aboutUs?.title}</p>
-      <p>{dict?.main?.aboutUs?.titleMo}</p>
-      <p>{dict?.main?.aboutUs?.text}</p>
-      <p>{dict?.main?.aboutUs?.info?.tit1}</p>
-      <Counter start={0} end={5000} duration={2} />
-      <p>{dict?.main?.aboutUs?.info?.tit2}</p>
-      <p>{dict?.main?.aboutUs?.info?.data2}</p>
+    <section className="aboutus main-section">
+      <div className="title-area text-white">
+        <p className="title">{dict?.main?.aboutUs?.title}</p>
+        <p className="!mt-5 text-4xl">A New History of Light</p>
+        <p className="!mt-6 text-xl opacity-50">
+          {dict?.main?.aboutUs?.text}
+        </p>{" "}
+        <Link
+          href={`${lang}/search/product`}
+          className="border border-white py-2 px-4 block mt-4 w-fit rounded-full color-whit mt-10"
+        >
+          Product Search
+        </Link>
+      </div>
+
+      <div className="bottom-area">
+        <div className="left">
+          <p className="text-green-500">{dict?.main?.aboutUs?.info?.tit1}</p>
+          <p className="mt-4 flex items-center gap-1 text-7xl">
+            <Counter start={0} end={5000} duration={2} /> +
+          </p>
+        </div>
+        <div className="right">
+          <p className="text-green-500">{dict?.main?.aboutUs?.info?.tit2}</p>
+          <p className="mt-4 flex items-center gap-1 text-7xl">
+            {dict?.main?.aboutUs?.info?.data2}
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
