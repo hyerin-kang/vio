8b2e70eee52a1565f87e50a1bb5f2e8387273840
[이모션]강혜린 <hkang0420@gmail.com>
2025-11-18 17:34:49 +0900

    🚧 aboutus-data

--- components/main/AboutUs.jsx
+++ components/main/AboutUs.jsx
@@ -1,7 +1,21 @@
+"use client";
 import Counter from "@/components/AnimateText/Counter";
+import { LangContext } from "@/context/LangContext";
+import { useContext } from "react";
 
 const AboutUs = () => {
-  return <Counter start={0} end={5000} duration={2} />;
+  const { dict } = useContext(LangContext);
+  return (
+    <section className="aboutus">
+      <p>{dict?.main?.aboutUs?.title}</p>
+      <p>{dict?.main?.aboutUs?.titleMo}</p>
+      <p>{dict?.main?.aboutUs?.text}</p>
+      <p>{dict?.main?.aboutUs?.info?.tit1}</p>
+      <Counter start={0} end={5000} duration={2} />
+      <p>{dict?.main?.aboutUs?.info?.tit2}</p>
+      <p>{dict?.main?.aboutUs?.info?.data2}</p>
+    </section>
+  );
 };
 
 export default AboutUs;
